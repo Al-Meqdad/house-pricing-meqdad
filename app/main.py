a4c9132909f6c9d16ab102b44c59b28768168343
AL-Meqdad Jabi <Mqdad1aljabi@gmail.com>
2021-12-25 19:31:06 +0200

    testing ports

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from flask import Flask, jsonify
-
+import os
 from src import Prediction_Apis
 
 app = Flask(__name__)
 
 app.register_blueprint(Prediction_Apis.Prediction_Api)
-
+PORT = 4000
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, port=os.environ.get('PORT', 5000), host="0.0.0.0")
