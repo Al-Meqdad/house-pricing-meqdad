8e09df53dd14bbf13196b46ddce8e665b049b519
AL-Meqdad Jabi <Mqdad1aljabi@gmail.com>
2021-12-25 19:18:31 +0200

    More editing

--- app/main.py
+++ app/main.py
@@ -7,4 +7,4 @@ app = Flask(__name__)
 app.register_blueprint(Prediction_Apis.Prediction_Api)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")
