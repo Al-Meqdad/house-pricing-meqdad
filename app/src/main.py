561eec8d88ef0c15a927ad8228ef2ed18b04a862
AL-Meqdad Jabi <Mqdad1aljabi@gmail.com>
2021-12-25 19:10:34 +0200

    e

--- app/src/main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, jsonify
-
-from Prediction_Apis import Prediction_Api
-app = Flask(__name__)
-
-app.register_blueprint(Prediction_Api)
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5000, host="0.0.0.0")
